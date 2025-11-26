fb73c7f6b7b52bf8e0ea187cf289449da445d540
master <ted3131@diquest.com>
2025-11-26 14:16:46 +0900

    commit

---
+++ src/main/java/com/example/shoppingjpa/ShoppingJpaApplication.java
@@ -0,0 +1,13 @@
+package com.example.shoppingjpa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShoppingJpaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ShoppingJpaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/shoppingjpa/ShoppingJpaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.shoppingjpa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShoppingJpaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
