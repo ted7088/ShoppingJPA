74fc6a243ffecf30aebdafafba37e3a77e6d98c7
master <ted3131@diquest.com>
2025-12-17 09:11:56 +0900

    commit

--- src/main/java/com/example/shoppingjpa/config/CustomUserDetailsService.java
+++ src/main/java/com/example/shoppingjpa/config/CustomUserDetailsService.java
@@ -15,9 +15,10 @@ import org.springframework.stereotype.Service;
 public class CustomUserDetailsService implements UserDetailsService {
 
     private final UserMapper userMapper;
-
+    //사용자관련 구현
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+
         User user = userMapper.findByUsername(username)
                 .orElseThrow(() -> new UsernameNotFoundException("사용자를 찾을 수 없습니다: " + username));
 
