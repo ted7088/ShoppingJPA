84f1ee5a58ffab600fa9a922ba1cd9564f7563bc
master <ted3131@diquest.com>
2025-12-24 10:37:19 +0900

    commit

--- src/main/java/com/example/shoppingjpa/config/WebConfig.java
+++ src/main/java/com/example/shoppingjpa/config/WebConfig.java
@@ -4,7 +4,7 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-
+//webconfig
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
 
